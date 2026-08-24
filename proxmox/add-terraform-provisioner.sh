0f70336a7c460da4970a6861aa3d811ccb5f976e
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2026-08-25 01:25:19 +0900

    fix: proxmox terraform provisioner SDN.use 권한 누락 추가

--- proxmox/add-terraform-provisioner.sh
+++ proxmox/add-terraform-provisioner.sh
@@ -7,7 +7,7 @@ pveum role add TerraformProvisioner -privs "VM.Allocate,VM.Clone,VM.Config.CDROM
 VM.Config.CPU,VM.Config.Cloudinit,VM.Config.Disk,VM.Config.Memory,\
 VM.Config.Network,VM.Config.Options,VM.Audit,VM.PowerMgmt,\
 Datastore.AllocateSpace,Datastore.AllocateTemplate,Datastore.Audit,\
-Sys.Audit,Sys.Console,Sys.Modify"
+Sys.Audit,Sys.Console,Sys.Modify,SDN.Use"
 
 # 2) 전용 사용자 생성 + 권한 부여
 pveum user add terraform@pve --comment "Terraform automation"
